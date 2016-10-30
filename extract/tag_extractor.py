d1890a34741fea74f985cf0a45a5925a8a5b99ef
AusarMon <zxc294811099@qq.com>
2016-10-30 15:09:00 +0800

    add extractor

--- extract/tag_extractor.py
+++ extract/tag_extractor.py
@@ -1,17 +1,29 @@
 # -*- coding: utf-8 -*-
 
-from Extractor import Extractor
-# from Feature_build import RawData
-import RawDataModel
+from extract import Extractor
+from extract import RawData
 
 class TagExtractor(Extractor):
 
+    file_name = "tag_cache_file"
+
+    @staticmethod
+    def init():
+        Extractor.create_file(file_name)
+
     @staticmethod
     def extract(line):
-        model = RawDataModel.RawData(line)
-        Extractor.creat_file("")
-        pass
+        model = RawData(line)
+        key = model.ip
+
+        # MEDIA_ID 
+        fields = list()
+        fields.append(model.media_id)
 
     @staticmethod
     def merge():
         pass
+
+    
+    if __name__ == '__main__':
+        TagExtractor.init()
